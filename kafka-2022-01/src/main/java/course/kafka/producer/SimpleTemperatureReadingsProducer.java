d91073a2a5d8a533a0129657a4a20895fc8fd5cd
Trayan Iliev <office@iproduct.org>
2022-04-18 14:05:06 +0300

    kafka-commands.txt added, producer added

--- kafka-2022-01/src/main/java/course/kafka/producer/SimpleTemperatureReadingsProducer.java
+++ kafka-2022-01/src/main/java/course/kafka/producer/SimpleTemperatureReadingsProducer.java
@@ -6,9 +6,14 @@ import lombok.val;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class SimpleTemperatureReadingsProducer implements Runnable {
@@ -16,18 +21,37 @@ public class SimpleTemperatureReadingsProducer implements Runnable {
     public static final String CLIENT_ID = "EventsClient";
     public static final String BOOTSTRAP_SERVERS = "localhost:9092";
 
-    private static Producer<String, TemperatureReading> createProducer() {
+    private static Producer<String, String> createProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return new KafkaProducer<String, TemperatureReading>(props);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        return new KafkaProducer<String, String>(props);
     }
 
     @Override
     public void run() {
         final var producer = createProducer();
         long time = System.currentTimeMillis();
+        new Random().doubles(10).map(t -> t * 40)
+                .mapToObj(t -> new TemperatureReading(UUID.randomUUID().toString(), "temperatureSensor01", t))
+                .map(reading -> new ProducerRecord(TOPIC, reading.getId(), reading))
+                .forEach(record -> {
+                    try {
+                        producer.send(record).get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        log.error("Error sending temperature reading", e);
+                    }
+                });
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        SimpleTemperatureReadingsProducer producer = new SimpleTemperatureReadingsProducer();
+        var executor = Executors.newCachedThreadPool();
+        var producerFuture = executor.submit(producer);
+        producerFuture.get();
+        executor.shutdown();
     }
 }
