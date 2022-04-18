ec8b3241e0242bb1e888a7ecd69270996dd0946c
Trayan Iliev <office@iproduct.org>
2022-04-18 15:46:08 +0300

    Json serializer and deserializer added

--- kafka2022-2-01-intro/src/main/java/course/kafka/SimpleTemperatureReadingsProducer.java
+++ kafka2022-2-01-intro/src/main/java/course/kafka/SimpleTemperatureReadingsProducer.java
@@ -14,6 +14,7 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.function.BiFunction;
 
 @Slf4j
 public class SimpleTemperatureReadingsProducer implements Runnable{
