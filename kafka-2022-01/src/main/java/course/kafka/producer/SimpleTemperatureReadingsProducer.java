9d24dcc48a889490bc7fc03617cbd9919a80511f
Trayan Iliev <office@iproduct.org>
2022-04-28 15:07:38 +0300

    starting multiple temperature readings producers callables.

--- kafka-2022-01/src/main/java/course/kafka/producer/SimpleTemperatureReadingsProducer.java
+++ kafka-2022-01/src/main/java/course/kafka/producer/SimpleTemperatureReadingsProducer.java
@@ -9,21 +9,31 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Properties;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import static course.kafka.model.TemperatureReading.HF_SENSOR_IDS;
+import static course.kafka.model.TemperatureReading.NORMAL_SENSOR_IDS;
 
 @Slf4j
-public class SimpleTemperatureReadingsProducer implements Runnable {
+public class SimpleTemperatureReadingsProducer implements Callable<String> {
     public static final String TOPIC = "temperature";
     public static final String CLIENT_ID = "EventsClient";
     public static final String BOOTSTRAP_SERVERS = "localhost:9093";
     public static final String HIGH_FREQUENCY_SENSORS = "sensors.highfrequency";
+    public static final int PRODUCER_TIMEOUT_MS = 20000;
+    private String sensorId;
+    private long maxDelayMs = 10000;
+    private int numReadings = 10;
+
+    public SimpleTemperatureReadingsProducer(String sensorId, long maxDelayMs, int numReadings) {
+        this.sensorId = sensorId;
+        this.maxDelayMs = maxDelayMs;
+        this.numReadings = numReadings;
+    }
 
     private static Producer<String, TemperatureReading> createProducer() {
         Properties props = new Properties();
@@ -42,41 +52,51 @@ public class SimpleTemperatureReadingsProducer implements Runnable {
         props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
         props.put(HIGH_FREQUENCY_SENSORS, HF_SENSOR_IDS.stream().collect(Collectors.joining(",")));
 
-
         return new KafkaProducer<>(props);
     }
 
     @Override
-    public void run() {
+    public String call() throws InterruptedException {
+        var latch = new CountDownLatch(numReadings);
         try (var producer = createProducer()) {
-            long time = System.currentTimeMillis();
-            var recordFutures = new Random().doubles(10).map(t -> t * 40)
-                    .mapToObj(t -> new TemperatureReading(UUID.randomUUID().toString(), "temperatureSensor01", t))
+            var count = new AtomicInteger();
+            new Random().doubles(numReadings).map(t -> t * 40)
+                    .peek(t -> {
+                        try {
+                            Thread.sleep((int) Math.random() * maxDelayMs);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .mapToObj(t -> new TemperatureReading(UUID.randomUUID().toString(), sensorId, t))
                     .map(reading -> new ProducerRecord(TOPIC, reading.getId(), reading))
                     .map(record -> {
                         return producer.send(record, ((metadata, exception) -> {
                             if (exception != null) {
                                 log.error("Error sending temperature readings", exception);
                             }
-                            log.info("Topic: {}, Partition: {}, Offset: {}, Timestamp: {}",
+                            log.info("SensorID: {}, Number: {}, Topic: {}, Partition: {}, Offset: {}, Timestamp: {}",
+                                    sensorId, count.incrementAndGet(),
                                     metadata.topic(), metadata.partition(), metadata.offset(), metadata.timestamp());
+                            latch.countDown();
                         }));
-                    }).collect(Collectors.toList());
-            recordFutures.forEach(f -> {
-                try {
-                    f.get();
-                } catch (InterruptedException | ExecutionException e) {
-                    log.error("Error sending temperature readings", e);
-                }
-            });
+                    }).forEach(future ->{});
         }
+        latch.await(PRODUCER_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        return sensorId;
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        SimpleTemperatureReadingsProducer producer = new SimpleTemperatureReadingsProducer();
+        final List<SimpleTemperatureReadingsProducer> producers = new ArrayList<>();
+        HF_SENSOR_IDS.forEach(hfs -> producers.add(new SimpleTemperatureReadingsProducer(hfs, 500, 20)));
+        NORMAL_SENSOR_IDS.forEach(ns -> producers.add(new SimpleTemperatureReadingsProducer(ns, 5000, 2)));
         var executor = Executors.newCachedThreadPool();
-        var producerFuture = executor.submit(producer);
-        producerFuture.get();
+        var ecs = new ExecutorCompletionService<String>(executor);
+        producers.stream().forEach(producer -> ecs.submit(producer));
+        for(int i = 0; i > producers.size(); i++){
+            System.out.printf("!!! Sensor '%s' completed.", ecs.take().get());
+        }
+
         executor.shutdownNow();
     }
 }
