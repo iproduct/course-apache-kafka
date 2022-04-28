f33f2940e746985ae6a0a56c62a73071c902a074
Trayan Iliev <office@iproduct.org>
2022-04-28 13:54:27 +0300

    dividing sensors to high frequency and low frequency

--- kafka-2022-01/src/main/java/course/kafka/model/TemperatureReading.java
+++ kafka-2022-01/src/main/java/course/kafka/model/TemperatureReading.java
@@ -6,12 +6,21 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TemperatureReading {
+    public static final List<String> HF_SENSOR_IDS = List.of(
+            "tHighFrequency-01", "tHighFrequency-02"
+    );
+    public static final List<String> NORMAL_SENSOR_IDS = List.of(
+            "tSensor-01", "tSensor-02", "tSensor-03", "tSensor-04", "tSensor-04", "tSensor-05",
+            "tSensor-06", "tSensor-07", "tSensor-08", "tSensor-09", "tSensor-10", "tSensor-11",
+            "tSensor-12", "tSensor-13", "tSensor-14", "tSensor-15", "tSensor-16", "tSensor-17"
+    );
     private String id;
     private String sensorId;
     private double value;
