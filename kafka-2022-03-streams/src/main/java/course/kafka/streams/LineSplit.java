2613a812ed01290c74ad7f0f2b352885dfe43bab
Trayan Iliev <office@iproduct.org>
2022-06-06 15:05:11 +0300

    kafka streams Pipe demos added

--- kafka-2022-03-streams/src/main/java/course/kafka/streams/LineSplit.java
+++ kafka-2022-03-streams/src/main/java/course/kafka/streams/LineSplit.java
@@ -5,24 +5,29 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class Pipe {
+public class LineSplit {
     public static void main(String[] args) {
         // 1) Configure stream
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, "exactly_once_v2");
-//        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, CustomTimeExtractor.class.getName());
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         // 2) Create stream builder
         final StreamsBuilder builder = new StreamsBuilder();
-        builder.stream("streams-input").to("streams-output");
+        KStream<String, String> stream = builder.stream("streams-input");
+        stream.flatMapValues(sentence ->
+                        Arrays.asList(sentence.toLowerCase(Locale.getDefault()).split("\\W+")))
+                .to("streams-output");
 
         // 3) Build stream topology
         final Topology topology = builder.build(); // build DAG
--- kafka-2022-03-streams/src/main/java/course/kafka/streams/WordCount.java
+++ kafka-2022-03-streams/src/main/java/course/kafka/streams/WordCount.java
@@ -1,28 +1,40 @@
 package course.kafka.streams;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class Pipe {
+public class WordCount {
     public static void main(String[] args) {
         // 1) Configure stream
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, "exactly_once_v2");
-//        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, CustomTimeExtractor.class.getName());
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         // 2) Create stream builder
         final StreamsBuilder builder = new StreamsBuilder();
-        builder.stream("streams-input").to("streams-output");
+        KStream<String, String> stream = builder.stream("streams-input");
+        stream.flatMapValues(sentence ->
+                        Arrays.asList(sentence.toLowerCase(Locale.getDefault()).split("\\W+")))
+                .groupBy((key, value) -> value)
+                .count(Materialized.as("word-counts-store"))
+                .toStream()
+                .mapValues((key, value) -> String.format("%-15s->%4d", key, value))
+                .to("latest-word-counts");
 
         // 3) Build stream topology
         final Topology topology = builder.build(); // build DAG
