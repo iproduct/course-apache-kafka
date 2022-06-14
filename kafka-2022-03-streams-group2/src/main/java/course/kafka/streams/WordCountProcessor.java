a6e98d58b65a809236373f4afadf23a677b94230
Trayan Iliev <office@iproduct.org>
2022-06-14 13:51:11 +0300

    stream joining demo setup - producers and consumers

--- kafka-2022-03-streams-group2/src/main/java/course/kafka/streams/WordCountProcessor.java
+++ kafka-2022-03-streams-group2/src/main/java/course/kafka/streams/WordCountProcessor.java
@@ -1,6 +1,7 @@
 package course.kafka.streams;
 
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.processor.PunctuationType;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.ProcessorContext;
@@ -52,5 +53,19 @@ public class WordCountProcessor implements Processor<String, String, String, Str
 
     @Override
     public void close() {
+    }
+
+    import java.time.Duration;
+    import org.apache.kafka.streams.kstream.TimeWindows;
+
+    public static void main(String[] args) {
+        // A tumbling time window with a size of 5 minutes (and, by definition, an implicit
+        // advance interval of 5 minutes), and grace period of 1 minute.
+        Duration windowSize = Duration.ofMinutes(5);
+        Duration gracePeriod = Duration.ofMinutes(1);
+        TimeWindows.ofSizeAndGrace(windowSize, gracePeriod);
+
+        // The above is equivalent to the following code:
+        TimeWindows.ofSizeAndGrace(windowSize, gracePeriod).advanceBy(windowSize);
     }
 }
